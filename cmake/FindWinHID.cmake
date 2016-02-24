d69d2a3ec381c96598cd12f06368ecc98bd9e745
Habegger Loïc <lhabegge@intranet.epfl.ch>
2016-02-24 14:32:33 +0100

    Updated WinHID CMake script to work with Win10

--- cmake/FindWinHID.cmake
+++ cmake/FindWinHID.cmake
@@ -25,6 +25,9 @@
 # (See accompanying file LICENSE_1_0.txt or copy at
 # http://www.boost.org/LICENSE_1_0.txt)
 
+# UPDATED BY loic.habegger@alumni.epfl.ch, https://github.com/Maaarmotte/wiiuse
+# Updated code was taken from https://github.com/janoc/wiiuse
+
 if(NOT WIN32)
 	find_package_handle_standard_args(WinHID
 		"Skipping search for Windows HID on non-Windows platform"
@@ -43,18 +46,24 @@ set(WINHID_ROOT_DIR
 	"Directory to search")
 
 if(CMAKE_SIZEOF_VOID_P MATCHES "8")
-	set(_arch amd64)
+	set(_arch amd64) # what the architecture used to be called
+	set(_arch8 x64) # what the WDK for Win8+ calls this architecture
 else()
-	set(_arch i386)
+	set(_arch i386) # what the architecture used to be called
+	set(_arch8 x86) # what the WDK for Win8+ calls this architecture
 endif()
 
+set(_deps_check)
+set(_need_crt_dir)
 if(MSVC)
 	include(PrefixListGlob)
 	include(CleanDirectoryList)
 	prefix_list_glob(_prefixed
 		"*/"
 		"$ENV{SYSTEMDRIVE}/WinDDK/"
-		"c:/WinDDK/")
+		"$ENV{ProgramFiles}/Windows Kits/"
+		"c:/WinDDK/"
+		"$ENV{SYSTEMDRIVE}/Program Files (x86)/Windows Kits/8.1")
 	clean_directory_list(_prefixed)
 	find_library(WINHID_LIBRARY
 		NAMES
@@ -68,27 +77,46 @@ if(MSVC)
 		"lib/wxp/${_arch}" # WinXP min requirement
 		"lib/wnet/${_arch}" # Win Server 2003 min requirement
 		"lib/wlh/${_arch}" # Win Vista ("Long Horn") min requirement
+		"lib/wlh/um/${_arch8}" # Win Vista ("Long Horn") min requirement
 		"lib/win7/${_arch}" # Win 7 min requirement
+		"lib/win7/um/${_arch8}" # Win 7 min requirement
+		"lib/win8/${_arch}" # Win 8 min requirement
+		"lib/win8/um/${_arch8}" # Win 8 min requirement
+		"lib/winv6.3/um/${_arch8}" # Win 8.1
 		)
-		# Might want to look close to the library first for the includes.
-	get_filename_component(_libdir "${WINHID_LIBRARY}" PATH)
-	get_filename_component(_basedir "${_libdir}/../../.." ABSOLUTE)
+	# Might want to look close to the library first for the includes.
+	if(WINHID_LIBRARY)
+		set(_prevdir)
+		get_filename_component(_basedir "${WINHID_LIBRARY}" PATH)
+		get_filename_component(_basedir2 "${_libdir}/../../../.." ABSOLUTE)
+		while(NOT IS_DIRECTORY "${_basedir}/lib" AND NOT (_basedir STREQUAL _prevdir))
+			set(_prevdir "${_basedir}")
+			get_filename_component(_basedir "${_basedir}/.." ABSOLUTE)
+		endwhile()
+	endif()
 
-	find_path(WINHID_CRT_INCLUDE_DIR # otherwise you get weird compile errors
-		NAMES
-		stdio.h
-		HINTS
-		"${_basedir}"
-		PATHS
-		"${WINHID_ROOT_DIR}"
-		PATH_SUFFIXES
-		inc/crt
-		NO_DEFAULT_PATH)
+	if(WINHID_LIBRARY AND EXISTS "${_basedir}/inc")
+		find_path(WINHID_CRT_INCLUDE_DIR # otherwise you get weird compile errors
+			NAMES
+			stdio.h
+			HINTS
+			"${_basedir}"
+			"${_basedir2}"
+			PATHS
+			"${WINHID_ROOT_DIR}"
+			PATH_SUFFIXES
+			inc/crt
+			NO_DEFAULT_PATH)
+		list(APPEND _deps_check WINHID_CRT_INCLUDE_DIR)
+		set(_need_crt_dir ON)
+	endif()
+
 	find_path(WINHID_INCLUDE_DIR
 		NAMES
 		hidsdi.h
 		HINTS
 		"${_basedir}"
+		"${_basedir2}"
 		PATHS
 		"${WINHID_ROOT_DIR}"
 		PATH_SUFFIXES
@@ -96,7 +124,8 @@ if(MSVC)
 		inc/api
 		inc/w2k
 		inc/wxp
-		inc/wnet)
+		inc/wnet
+		include/shared)
 else()
 	find_library(WINHID_LIBRARY
 		NAMES
@@ -117,13 +146,20 @@ else()
 		include/w32api/ddk
 		include/ddk
 		ddk)
+		
+	# kludge to set the value to something sensible for Win8.1 kit which doesn't
+	# have stdio.h :(
+	if(NOT ${WINHID_CRT_INCLUDE_DIR})
+		set(WINHID_CRT_INCLUDE_DIR ${WINHID_INCLUDE_DIR})
+	endif()
 endif()
 
 include(FindPackageHandleStandardArgs)
 find_package_handle_standard_args(WinHID
 	DEFAULT_MSG
 	WINHID_LIBRARY
-	WINHID_INCLUDE_DIR)
+	WINHID_INCLUDE_DIR
+	${_deps_check})
 
 if(WINHID_FOUND)
 	if(MSVC)
@@ -138,6 +174,8 @@ if(WINHID_FOUND)
 			set(_winreq "Windows Vista")
 		elseif(WINHID_LIBRARY MATCHES "lib/win7")
 			set(_winreq "Windows 7")
+		elseif(WINHID_LIBRARY MATCHES "lib/win8")
+			set(_winreq "Windows 8 (Possibly?)")
 		endif()
 		if(NOT "${WINHID_MIN_WINDOWS_VER}" STREQUAL "${_winreq}")
 			if(NOT WinHID_FIND_QUIETLY)
@@ -146,15 +184,17 @@ if(WINHID_FOUND)
 			endif()
 			set(WINHID_MIN_WINDOWS_VER "${_winreq}" CACHE INTERNAL "" FORCE)
 		endif()
-		
+	endif()
+	set(WINHID_LIBRARIES "${WINHID_LIBRARY}")
+	if(_need_crt_dir)
 		set(WINHID_INCLUDE_DIRS
 			"${WINHID_CRT_INCLUDE_DIR}"
 			"${WINHID_INCLUDE_DIR}")
 	else()
+		# Don't need that CRT include dir for WDK 8+
 		set(WINHID_INCLUDE_DIRS
 			"${WINHID_INCLUDE_DIR}")
 	endif()
-	set(WINHID_LIBRARIES "${WINHID_LIBRARY}")
 	mark_as_advanced(WINHID_ROOT_DIR)
 endif()
 
